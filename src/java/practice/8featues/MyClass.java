05d8f2b71993b483676dd8258a962fadb3110df5
sobhan chitrada <sobhan.chitrada@gmail.com>
2022-02-18 19:28:52 -0500

    some changes

---
+++ src/java/practice/8featues/MyClass.java
@@ -0,0 +1,16 @@
+public class MyClass implements Interface1, Interface2 {
+ 
+    @Override
+    public void method2() {
+    }
+ 
+    @Override
+    public void method1(String str) {
+    }
+ 
+
+    public  static void main(String args[]){
+        MyClass m =new MyClass();
+        m.log("sobhan");
+    }
+}
